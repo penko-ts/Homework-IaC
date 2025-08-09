ccf93b313bf7fec09f361752ab4e1febb4c5cf2d
penko-ts <penko.aprilci@gmail.com>
2025-08-09 21:58:41 +0300

    Repo init

--- lib/homework-ia_c-stack.ts
+++ lib/homework-ia_c-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
+import { Protocol } from 'aws-cdk-lib/aws-ec2';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Subscription, Topic, SubscriptionProtocol } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path from 'path';
 
-export class HomeworkIaCStack extends cdk.Stack {
+export class EmptyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const WebsiteBucket = new Bucket(this, 'staticWebsiteBucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HomeworkIaCQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const topic = new Topic(this, 'errorTopic',);
+
+    const subscription = new Subscription(this, 'errorSubscription', {
+      topic: topic,
+      protocol: SubscriptionProtocol.EMAIL,
+      endpoint: 'penko.aprilci@gmail.com'
+    });
+
+    const bucketDeployment = new BucketDeployment(this, 'indexDeployment', {
+      sources: [Source.asset(path.join(__dirname, '../website-assets'))],
+      destinationBucket: WebsiteBucket
+    });  
+
+    new CfnOutput(this, 'url', {
+      key: 'websiteUrl',
+      value: WebsiteBucket.bucketWebsiteUrl + '/index.html',
+    });
   }
 }
--- test/homework-ia_c.test.ts
+++ test/homework-ia_c.test.ts
@@ -1,17 +1,12 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as HomeworkIaC from '../lib/homework-ia_c-stack';
+import * as cdk from "aws-cdk-lib";
+import * as EmptyCdkProject from "../lib/homework-ia_c-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/homework-ia_c-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new HomeworkIaC.HomeworkIaCStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+    const app = new cdk.App();
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    const stack = new EmptyCdkProject.EmptyCdkProjectStack(app, 'TestStack');
+    
+    expect(stack).toMatchSnapshot();
+    
+    
 });
